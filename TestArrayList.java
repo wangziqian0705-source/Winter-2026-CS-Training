a163335c5376b3f0bcb66321725e6c316d890065
wangziqian0705-source <wangziqian0705@gmail.com>
2026-01-07 11:50:57 +0800

    Add files via upload

--- TestArrayList.java
+++ TestArrayList.java
@@ -1,2 +1,11 @@
-public class TestArrayList {
-}
+import java.util.ArrayList;
+    public class TestArrayList {
+        public static void main(String[] args) {
+            // 创建一个空列表
+            ArrayList<Integer> list = new ArrayList<>();
+            // 疯狂添加数据，触发扩容
+            for (int i = 0; i < 20; i++) {
+                list.add(i);
+            }
+        }
+    }
